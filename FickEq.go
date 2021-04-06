664fd94c3636db2449e40faae8e340f7b7325c50
Bogdan <bogdan.kocsis@gmail.com>
2021-04-06 11:04:01 +0300

    Refactored some values from FickEq.go.

--- FickEq.go
+++ FickEq.go
@@ -8,106 +8,137 @@ package main
 
 import (
 	"fmt"
-	"gonum.org/v1/gonum/mat"
+	//"gonum.org/v1/gonum/mat"
 )
 
 func main() {
 
 	var sus0 = 60.0
-	var inf0 = 0
+	//var inf0 = 0
 	a := 20.0 / 50.0
+	fmt.Println(a)
 	b := 1.0
+	fmt.Println(b)
 	var D = 1000.0
+	fmt.Println(D)
 	var n = 200
-	var nn = 2*n + 4
+	var nn = 2*n + 2
 	var maxk = 100
 	var L = 900
 	var dx = float64(L) / float64(n)
-	x := make([][]float64, n)
-	for i := range x {
-		x[i] = make([]float64, n)
+	fmt.Println(dx)
+	x := make([]float64, n+1)
+
+	for i := 0; i < n+1; i++ {
+		x[i] = float64(i) * dx
 	}
-	for i := 0; i < n; i++ {
-		x[i][i] = 0
-		for j := 0; j < n; j++ {
-			if i != j {
-				x[i][j] = float64(i+j) * dx
-			}
-		}
-	}
+
+	fmt.Println(x)
 	var T = 3
 	dt := float64(T) / float64(maxk)
 	alpha := D * dt / (dx * dx)
+	fmt.Println(alpha)
 	FP := make([][]float64, nn)
 	for i := range FP {
 		FP[i] = make([]float64, nn)
 	}
+	fmt.Println(len(FP))
 
 	F := make([][1]float64, nn)
+	fmt.Println(len(F))
 
-	sus := make([][1]float64, n+2) // define initial populations
-	for i := 0; i < n+2; i++ {
+	sus := make([][1]float64, n+1) // define initial populations
+	for i := 0; i < n+1; i++ {
 		sus[i][0] = 1 * sus0
 	}
-	for i := 1; i <= 3; i++ {
+	fmt.Println(len(sus))
+
+	for i := 0; i < 3; i++ {
 		sus[i][0] = 2
 	}
 
 	var susp = sus
-	inf := make([][1]float64, n+2)
-	for i := 0; i < n+2; i++ {
-		inf[i][0] = float64(1 * inf0)
-	}
-	for i := 1; i <= 3; i++ {
+	fmt.Println(len(susp))
+
+	inf := make([][1]float64, n+1)
+
+	for i := 0; i < 3; i++ {
 		inf[i][0] = 48
 	}
+	fmt.Println(len(inf))
+
 	var infp = inf
 
 	time := make([][]float64, maxk)
 	for i := range time {
 		time[i] = make([]float64, maxk)
 	}
-	sustime := make([][]float64, maxk)
+	fmt.Println(len(time))
+
+	sustime := make([][]float64, n+1)
 	for i := range sustime {
-		sustime[i] = make([]float64, n+1)
+		sustime[i] = make([]float64, maxk)
 	}
-	inftime := make([][]float64, maxk)
+	fmt.Println(len(sustime))
+
+	inftime := make([][]float64, n+1)
 	for i := range inftime {
-		inftime[i] = make([]float64, n+1)
+		inftime[i] = make([]float64, maxk)
+	}
+	fmt.Println(len(inftime))
+	u := make([][1]float64, 2*(n+1))
+	for i := 0; i < n+1; i++ {
+		u[i][0] = susp[i][0]
+	}
+	var j = 0
+	fmt.Println("u:", len(u))
+	for i := n + 1; i < 2*(n+1); i++ {
+		u[i][0] = infp[j][0]
+		j = j + 1
 	}
 
 	for k := 0; k < maxk; k++ {
-		var aux []float64
-		for _, arr := range susp {
-			for _, item := range arr {
-				aux = append(aux, item)
-			}
-		}
-		for _, arr := range infp {
-			for _, item := range arr {
-				aux = append(aux, item)
-			}
-		}
+		//var aux []float64
+		//for _, arr := range susp {
+		//	for _, item := range arr {
+		//		aux = append(aux, item)
+		//	}
+		//}
+		//for _, arr := range infp {
+		//	for _, item := range arr {
+		//		aux = append(aux, item)
+		//	}
+		//}
 
-		u := mat.NewDense(2*(n+2), 1, aux)
+		//	u := mat.NewDense(2*(n+2), 1, aux)
+		u := make([][1]float64, 2*(n+1))
+		for i := 0; i < n+1; i++ {
+			u[i][0] = susp[i][0]
+		}
+		var j = 0
+		fmt.Println("u:", len(u))
+		for i := n + 1; i < 2*(n+1); i++ {
+			u[i][0] = infp[j][0]
+			j = j + 1
+		}
 		m := 1
-		var errors = 0.0
+		//var errors = 0.0
 		for m = 0; m < 20; m++ {
 			for i := 0; i < nn; i++ {
-				if i >= 1 && i <= n {
+				if i >= 0 && i < n {
 					F[i][0] = sus[i][0] - susp[i][0] + dt*a*sus[i][0]*inf[i][0]
 					FP[i][i] = 1 + dt*a*inf[i][0]
 					FP[i][i+n+1] = dt * a * sus[i][0]
 				}
-				if i == n+2 {
+				if i == n+1 {
 					F[i][0] = inf[1][0] - infp[1][0] + b*dt*inf[1][0] - alpha*2*(-inf[1][0]+inf[2][0]) - a*dt*sus[1][0]*inf[1][0]
 					FP[i][i] = 1 + b*dt + alpha*2 - a*dt*sus[1][0]
 					FP[i][i+1] = -2 * alpha
 					FP[i][1] = -a * dt * inf[1][0]
 
 				}
-				if i > n+2 && i < nn {
-					i_shift := i - (n + 2)
+				if i > n+1 && i < nn {
+					i_shift := i - (n + 1)
 					F[i][0] = inf[i_shift][0] - infp[i_shift][0] + b*dt*inf[i_shift][0] - alpha*(inf[i_shift-1][0]-2*inf[i_shift][0]+inf[i_shift][0]) - a*dt*sus[i_shift][0]*inf[i_shift][0]
 					FP[i][i] = 1 + b*dt + alpha*2 - a*dt*sus[i_shift][0]
 					FP[i][i-1] = -alpha
@@ -122,60 +153,64 @@ func main() {
 				}
 			}
 
-			var result []float64
-			for _, arr := range FP {
-				for _, item := range arr {
-					result = append(result, item)
-				}
-			}
-
-			z := mat.NewDense(nn, nn, result)
-			var du mat.Dense
-			_ = du.Inverse(z)
-
-			//if err != nil {
-			//	log.Fatalf("z is not invertible: %v", err)
+			//
+			//		var result []float64
+			//		for _, arr := range FP {
+			//			for _, item := range arr {
+			//				result = append(result, item)
+			//			}
+			//		}
+			//
+			//		z := mat.NewDense(nn, nn, result)
+			//		var du mat.Dense
+			//		_ = du.Inverse(z)
+			//
+			//		//if err != nil {
+			//		//	log.Fatalf("z is not invertible: %v", err)
+			//		//}
+			//
+			//		u.Sub(u, &du)
+			//		for i := 1; true; {
+			//			sus[i][0] = u.At(i, 0)
+			//		}
+			//		var j = n + 2
+			//		for i := 1; true; {
+			//			if j < nn {
+			//				inf[i][0] = u.At(j, 0)
+			//				j = j + 1
+			//			}
+			//		}
+			//
+			//		var result1 []float64
+			//		for _, arr := range F {
+			//			for _, item := range arr {
+			//				result = append(result1, item)
+			//			}
+			//		}
+			//		l := mat.NewDense(nn, 1, result)
+			//		errors = mat.Norm(l, 2)
+			//		if errors < 0.0001 {
+			//			break
+			//		}
+			//
+			//	}
+			//
+			//	time[k][k] = float64(k) * dt
+			//	fmt.Println(time[k][k])
+			//	fmt.Println(m)
+			//	fmt.Println(errors)
+			//	susp = sus
+			//	infp = inf
+			//	for i := range sustime {
+			//		sustime[i][k] = sus[i][k]
+			//	}
+			//	for i := range inftime {
+			//		inftime[i][k] = inf[i][k]
+			//	}
+			//
 			//}
-
-			u.Sub(u, &du)
-			for i := 1; true; {
-				sus[i][0] = u.At(i, 0)
-			}
-			var j = n + 2
-			for i := 1; true; {
-				if j < nn {
-					inf[i][0] = u.At(j, 0)
-					j = j + 1
-				}
-			}
-
-			var result1 []float64
-			for _, arr := range F {
-				for _, item := range arr {
-					result = append(result1, item)
-				}
-			}
-			l := mat.NewDense(nn, 1, result)
-			errors = mat.Norm(l, 2)
-			if errors < 0.0001 {
-				break
-			}
-
-		}
-
-		time[k][k] = float64(k) * dt
-		fmt.Println(time[k][k])
-		fmt.Println(m)
-		fmt.Println(errors)
-		susp = sus
-		infp = inf
-		for i := range sustime {
-			sustime[i][k] = sus[i][k]
-		}
-		for i := range inftime {
-			inftime[i][k] = inf[i][k]
 		}
 
 	}
-
+	fmt.Println(FP)
 }
--- Test.go
+++ Test.go
@@ -1,7 +1,106 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func sum(array []int) int { // used to add up the initial people sick during epidemic seeding
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
+
+// Population is stored in a list.  The list size variable is "size". You can
+// adjust this and all the other parameters are calculated automatically
+// Variables:
+//	size: the size of the population can be anynumber 1000 works good
+//	seed: the initial number of sick people in the population to get the epidemic started
+//	days: the number of days to run the simulation. Should start at least at 50
+//	daystorecovery: average days for a normal person to recover and become immune
+//	PeopleContact: typical number of people a sick person will come in contact with per day
+// 	targetperson: a person choosen at random in the population and if not sick, we try to get them sick
+//	ProbOfInfection: each non-sick person who encounters a sick person has a probability of getting sick
+//	RO: the reproduction rate. How many people a sick person will get sick
+//	History: An array where we store the day results in the epidemic lifecycle
+// 	sick: A list that is variable size long. Stores a person's status 0: Not sick, 1: sick, 2: immune
+//	dayssick: A list where the days each person is sick is stored. Same index as list: sick
 
 func main() {
-	fmt.Println("Hello, 世界")
+	var size, seed, days, daystorecovery, PeopleContact int
+	var targetperson int
+	var ProbOfInfection, R0 float32
+	var history [200][3]int // the size of this array needs to at least as
+
+	size = 10000
+	seed = 5
+	days = 125
+	daystorecovery = 10
+	R0 = 4.0
+	PeopleContact = 10
+	ProbOfInfection = 100 * R0 / (float32(PeopleContact) * float32(daystorecovery))
+
+	// big as variable: days
+	// [day][# not sick][# sick][# immune]
+	sick := make([]int, size)
+	dayssick := make([]int, size)
+
+	fmt.Println("Prob:", ProbOfInfection)
+
+	rand.Seed(time.Now().UnixNano()) // randomly get "seed" number of people sick
+	for i := 0; i < seed; i++ {
+		sick[rand.Intn(size)] = 1
+	}
+
+	history[0][0] = size - sum(sick) // set up history for day=0
+	history[0][1] = sum(sick)
+	// print out day=0 score
+	// this is printed as a string suitable for importing to excel for graphing as a csv file
+	fmt.Println("0,", history[0][0], ",", history[0][1], ",", history[0][2])
+
+	for day := 1; day < days; day++ { // loop for the simulation days
+		history[day][0] = history[day-1][0] // carry over yesterdays totals
+		history[day][1] = history[day-1][1]
+		history[day][2] = history[day-1][2]
+
+		for i := 0; i < size; i++ { // loop through the population
+			if sick[i] == 1 { // if sick
+				dayssick[i]++                     // increment days sick
+				if dayssick[i] > daystorecovery { // if reached recovery days
+					sick[i] = 2       // then change status to recovered (2)
+					history[day][2]++ // increment # of people recovered
+					history[day][1]-- // decrement # of people sick
+				}
+				for x := 0; x < PeopleContact; x++ { // while with a sick person
+					targetperson = rand.Intn(size) // find people whom they might infect
+					// pick people in the sick list at random
+					if i != targetperson && sick[targetperson] == 0 { // available to be sick, roll the dice
+						tar := rand.Intn(100)           // get a number from 0 to 100
+						if tar < int(ProbOfInfection) { // if < than ProbOfInfection calculated above
+							sick[targetperson] = 1 // lucky them they are now sick
+							history[day][0]--      // decrement # of people not sick
+							history[day][1]++      // increment # of people sick
+						}
+					}
+				}
+			}
+		}
+		// print out the day's score
+		fmt.Println(day, ",", history[day][0], ",", history[day][1], ",", history[day][2])
+	}
+
+	x := make([]float64, days)
+	y0 := make([]float64, days)
+	y1 := make([]float64, days)
+	y2 := make([]float64, days)
+	for i := 0; i < days; i++ {
+		x[i] = float64(i)
+		y0[i] = float64(history[i][0])
+		y1[i] = float64(history[i][1])
+		y2[i] = float64(history[i][2])
+	}
+
 }
